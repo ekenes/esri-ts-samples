29e70b33b838631be38cac2432941b78388c7cde
Kristian Ekenes <KEkenes@esri.com>
2018-09-27 08:43:24 -0700

    Interactive Arcade. Initial commit.

---
+++ visualization/updating-arcade/app/main.ts
@@ -0,0 +1,19 @@
+import esri = __esri;
+
+import EsriMap = require("esri/Map");
+import MapView = require("esri/views/MapView");
+
+( async () => {
+
+  const map = new EsriMap({
+    basemap: "streets"
+  });
+
+  const view = new MapView({
+    map: map,
+    container: "viewDiv",
+    center: [ -118.244, 34.052],
+    zoom: 12
+  });
+
+});
