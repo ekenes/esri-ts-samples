9d6aa912e619f7d7bc17e8086c463b5f5ad11ee1
Kristian Ekenes <KEkenes@esri.com>
2018-09-27 17:15:57 -0700

    Add js function app

---
+++ visualization/js-functions/app/main.js
@@ -0,0 +1,172 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+define(["require", "exports", "esri/WebMap", "esri/views/MapView", "esri/renderers", "esri/symbols", "esri/widgets/Legend"], function (require, exports, WebMap, MapView, renderers_1, symbols_1, Legend) {
+    "use strict";
+    var _this = this;
+    Object.defineProperty(exports, "__esModule", { value: true });
+    (function () { return __awaiter(_this, void 0, void 0, function () {
+        function updateRenderer(name) {
+            var oldRenderer = chinaLayer.renderer;
+            var newRenderer = oldRenderer.clone();
+            newRenderer.uniqueValueInfos.forEach(function (info, i) {
+                info.label = (i !== 1) ? info.label.substring(0, 5) + " " + value + "%" : "Within +/- 1% of " + name + " (" + value + "%)";
+            });
+            newRenderer.legendOptions = {
+                title: "% population without formal education relative to " + name
+            };
+            chinaLayer.renderer = newRenderer;
+        }
+        function valueFunction(graphic) {
+            var attributes = graphic.attributes;
+            var total = attributes.EDUC01_CY + attributes.EDUC02_CY + attributes.EDUC03_CY
+                + attributes.EDUC04_CY + attributes.EDUC05_CY + attributes.EDUC06_CY;
+            var percentTotalFeature = Math.round((attributes[visualizationField] / total) * 100);
+            var category = "";
+            if (percentTotalFeature <= value + 1 && percentTotalFeature >= value - 1) {
+                category = "Similar";
+            }
+            else if (percentTotalFeature > value) {
+                category = "Above";
+            }
+            else {
+                category = "Below";
+            }
+            return category;
+        }
+        function createSymbol(color, style) {
+            return new symbols_1.SimpleFillSymbol({
+                color: color,
+                outline: {
+                    color: "white",
+                    width: 0.75
+                },
+                style: style ? style : "solid"
+            });
+        }
+        var visualizationField, allFields, description, value, map, view, chinaLayer;
+        var _this = this;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    visualizationField = "EDUC01_CY";
+                    allFields = [
+                        "EDUC01_CY",
+                        "EDUC02_CY",
+                        "EDUC03_CY",
+                        "EDUC04_CY",
+                        "EDUC05_CY",
+                        "EDUC06_CY" // University and Above
+                    ];
+                    description = "Education in mainland China";
+                    value = 5;
+                    map = new WebMap({
+                        portalItem: {
+                            id: "b8f443e2378344e79566fa64430a3c25"
+                        }
+                    });
+                    view = new MapView({
+                        map: map,
+                        container: "viewDiv",
+                        popup: {
+                            dockEnabled: true,
+                            dockOptions: {
+                                position: "top-right",
+                                breakpoint: false
+                            }
+                        },
+                        highlightOptions: {
+                            color: [0, 0, 0],
+                            fillOpacity: 0
+                        }
+                    });
+                    view.ui.add(new Legend({ view: view }), "bottom-left");
+                    return [4 /*yield*/, view.when()];
+                case 1:
+                    _a.sent();
+                    chinaLayer = map.layers.getItemAt(0);
+                    chinaLayer.popupEnabled = false;
+                    chinaLayer.when();
+                    chinaLayer.title = "" + description;
+                    chinaLayer.renderer = new renderers_1.UniqueValueRenderer({
+                        field: valueFunction,
+                        legendOptions: {
+                            title: "% population without formal education"
+                        },
+                        uniqueValueInfos: [{
+                                value: "Above",
+                                symbol: createSymbol("#b30000"),
+                                label: "Above " + value + "%"
+                            }, {
+                                value: "Similar",
+                                symbol: createSymbol("gray"),
+                                label: "Similar (within +/- 1%)"
+                            }, {
+                                value: "Below",
+                                symbol: createSymbol("orange"),
+                                label: "Below " + value + "%"
+                            }],
+                        defaultLabel: "No data",
+                        defaultSymbol: createSymbol("black", "backward-diagonal")
+                    });
+                    view.on("click", function (event) { return __awaiter(_this, void 0, void 0, function () {
+                        var hitTestResponse, result, attributes, newValue, totalValue, name;
+                        return __generator(this, function (_a) {
+                            switch (_a.label) {
+                                case 0: return [4 /*yield*/, view.hitTest(event)];
+                                case 1:
+                                    hitTestResponse = _a.sent();
+                                    result = hitTestResponse.results && hitTestResponse.results.filter(function (result) {
+                                        return result.graphic.layer.type === "feature";
+                                    })[0];
+                                    attributes = result.graphic.attributes;
+                                    newValue = attributes[visualizationField];
+                                    totalValue = 0;
+                                    allFields.forEach(function (fieldName) {
+                                        totalValue += attributes[fieldName];
+                                    });
+                                    value = Math.round((newValue / totalValue) * 100);
+                                    name = attributes.NAME;
+                                    updateRenderer(name);
+                                    return [2 /*return*/];
+                            }
+                        });
+                    }); });
+                    return [2 /*return*/];
+            }
+        });
+    }); })();
+});
+//# sourceMappingURL=main.js.map
---
+++ visualization/js-functions/app/main.ts
@@ -0,0 +1,137 @@
+import esri = __esri;
+
+import WebMap = require("esri/WebMap");
+import MapView = require("esri/views/MapView");
+import { UniqueValueRenderer } from "esri/renderers";
+import { SimpleFillSymbol } from "esri/symbols";
+
+import Legend = require("esri/widgets/Legend");
+
+( async () => {
+
+  const visualizationField = "EDUC01_CY";
+  const allFields = [
+    "EDUC01_CY",  // no education
+    "EDUC02_CY",  // Primary School
+    "EDUC03_CY",  // Junior Middle School
+    "EDUC04_CY",  // Senior High School
+    "EDUC05_CY",  // Junior College
+    "EDUC06_CY"  // University and Above
+  ];
+  const description = "Education in mainland China";
+  let value = 5;
+
+  const map = new WebMap({
+    portalItem: {
+      id: "b8f443e2378344e79566fa64430a3c25"
+    }
+  });
+
+  const view = new MapView({
+    map: map,
+    container: "viewDiv",
+    popup: {
+      dockEnabled: true,
+      dockOptions: {
+        position: "top-right",
+        breakpoint: false
+      }
+    },
+    highlightOptions: {
+      color: [ 0, 0, 0 ],
+      fillOpacity: 0
+    }
+  });
+  view.ui.add(new Legend({ view }), "bottom-left");
+
+  await view.when();
+
+  const chinaLayer = map.layers.getItemAt(0) as esri.FeatureLayer;
+  chinaLayer.popupEnabled = false;
+  chinaLayer.when();
+
+  chinaLayer.title = `${description}`;
+  chinaLayer.renderer = new UniqueValueRenderer({
+    field: valueFunction,
+    legendOptions: {
+      title: "% population without formal education"
+    },
+    uniqueValueInfos: [{ 
+      value: "Above",
+      symbol: createSymbol("#b30000"),
+      label: `Above ${value}%`
+    }, {
+      value: "Similar",
+      symbol: createSymbol("gray"),
+      label: `Similar (within +/- 1%)`
+    }, {
+      value: "Below",
+      symbol: createSymbol("orange"),
+      label: `Below ${value}%`
+    }],
+    defaultLabel: "No data",
+    defaultSymbol: createSymbol("black", "backward-diagonal")
+  })
+
+  view.on("click", async (event) => {
+    const hitTestResponse = await view.hitTest(event);
+    const result = hitTestResponse.results && hitTestResponse.results.filter( result => {
+      return result.graphic.layer.type === "feature";
+    })[0];
+
+    const attributes = result.graphic.attributes;
+    const newValue = attributes[visualizationField];
+    let totalValue = 0;
+    
+    allFields.forEach( fieldName => {
+      totalValue += attributes[fieldName];
+    });
+
+    value = Math.round(( newValue / totalValue ) * 100);
+    const name = attributes.NAME;
+    updateRenderer( name );
+  });
+
+  function updateRenderer (name: string) {
+    const oldRenderer = chinaLayer.renderer as UniqueValueRenderer;
+    const newRenderer = oldRenderer.clone();
+    newRenderer.uniqueValueInfos.forEach( (info, i) => {
+      info.label = ( i !== 1 ) ? `${info.label.substring(0,5)} ${value}%` : `Within +/- 1% of ${name} (${value}%)`;
+    });
+    newRenderer.legendOptions = {
+      title: `% population without formal education relative to ${name}`
+    };
+    chinaLayer.renderer = newRenderer;
+  }
+
+  function valueFunction(graphic: esri.Graphic): string {
+    const attributes = graphic.attributes;
+    const total = attributes.EDUC01_CY + attributes.EDUC02_CY + attributes.EDUC03_CY 
+      + attributes.EDUC04_CY + attributes.EDUC05_CY + attributes.EDUC06_CY;
+
+    const percentTotalFeature = Math.round( ( attributes[visualizationField] / total) * 100);
+    let category = "";
+    if (percentTotalFeature <= value + 1 && percentTotalFeature >= value - 1){
+      category = "Similar";
+    }
+    else if (percentTotalFeature > value){
+      category = "Above";
+    }
+    else {
+      category = "Below";
+    }
+    return category;
+  }
+
+  function createSymbol (color: any, style?: string): SimpleFillSymbol {
+    return new SimpleFillSymbol({
+      color,
+      outline: {
+        color: "white",
+        width: 0.75
+      },
+      style: style ? style : "solid"
+    });
+  }
+
+})();
