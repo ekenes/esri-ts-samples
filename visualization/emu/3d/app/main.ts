c3f3b7bf782c9c5d2cf53878520447a57c0dde8e
Kristian Ekenes <KEkenes@esri.com>
2019-01-22 16:32:33 -0800

    initial 3d commit

---
+++ visualization/emu/3d/app/main.ts
@@ -0,0 +1,77 @@
+import esri = __esri;
+import EsriMap = require("esri/Map");
+import MapView = require("esri/views/MapView");
+import FeatureLayer = require("esri/layers/FeatureLayer");
+
+( async () => {
+
+  const url = "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/EMU_master_3857_2/FeatureServer/0";
+
+  const layer = new FeatureLayer({
+    url
+  });
+  const map = new EsriMap({
+    basemap: "dark-gray-vector",
+    layers: [ layer ]
+  });
+
+  const view = new MapView({
+    map: map,
+    container: "viewDiv",
+    extent: {
+      "spatialReference": {
+        "wkid": 3857
+      },
+      "xmin": -32607543,
+      "ymin": -148400,
+      "xmax": -31196210,
+      "ymax": 952292
+    }
+  });
+
+  await view.when();
+
+  const depthSlider = document.getElementById("depth-slider") as HTMLInputElement;
+  view.ui.add("slider-div", "top-right");
+
+  const sliderValueText = document.getElementById("slider-value-text");
+
+ 
+
+  const query = layer.createQuery();
+  query.outFields = [ "UnitTop" ];
+  query.returnDistinctValues = true;
+  query.returnGeometry = false;
+  console.log(query);
+
+  const uniqueDepthValues = await layer.queryFeatures(query).then( response => {
+    return response.features.map( feature => feature.attributes.UnitTop );
+  });
+
+  console.log("unique values: ", uniqueDepthValues);
+
+  const layerView = await view.whenLayerView(layer);
+  filterByDepth();
+
+  depthSlider.addEventListener("input", filterByDepth);
+
+  function filterByDepth(){
+    const sliderValue = parseInt(depthSlider.value);
+    sliderValueText.innerHTML = depthSlider.value;
+
+    const sortedValuesByDifference = uniqueDepthValues.map( value => {
+      return {
+        difference: Math.abs(value - sliderValue),
+        value
+      }
+    }).sort( (a, b) => {
+      return a.difference - b.difference; 
+    });
+
+    layerView.filter = {
+      where: `UnitTop = ${sortedValuesByDifference[0].value}`
+    };
+  }
+
+  
+})();
