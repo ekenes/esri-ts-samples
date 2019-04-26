94bfb59855c9d988ec565248334f0c404a91095f
Kristian Ekenes <KEkenes@esri.com>
2019-04-25 17:14:49 -0700

    initial commit

---
+++ widgets/search/custom-source/app/main.js
@@ -0,0 +1,253 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/widgets/Search", "esri/request", "esri/widgets/Search/SearchSource", "esri/geometry/projection", "esri/PopupTemplate", "esri/Graphic", "esri/geometry"], function (require, exports, EsriMap, MapView, Search, esriRequest, SearchSource, projection, PopupTemplate, Graphic, geometry_1) {
+    "use strict";
+    var _this = this;
+    Object.defineProperty(exports, "__esModule", { value: true });
+    try {
+        (function () { return __awaiter(_this, void 0, void 0, function () {
+            function searchPlaceNames(propertyName, suggestMunicipality, suggestCounty) {
+                return __awaiter(this, void 0, void 0, function () {
+                    var searchUrl, searchResponse, xmlResponse, jsResponse, rawResults, searchResults;
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0:
+                                searchUrl = "https://ws.geonorge.no/SKWS3Index/ssr/sok";
+                                return [4 /*yield*/, esriRequest(searchUrl, {
+                                        responseType: "xml",
+                                        query: {
+                                            navn: propertyName
+                                        }
+                                    })];
+                            case 1:
+                                searchResponse = _a.sent();
+                                xmlResponse = searchResponse.data;
+                                jsResponse = window.xmlToJSON.parseXML(xmlResponse);
+                                console.log("js response", jsResponse);
+                                rawResults = jsResponse.sokRes[0].stedsnavn;
+                                searchResults = rawResults.map(function (item) {
+                                    var x = item.aust[0]._text;
+                                    var y = item.nord[0]._text;
+                                    var name = item.stedsnavn[0]._text;
+                                    var ssrId = item.ssrId[0]._text;
+                                    var status = item.skrivemaatestatus[0]._text;
+                                    var SpName = item.skrivemaatenavn[0]._text;
+                                    var type = item.navnetype[0]._text;
+                                    var municipality = item.kommunenavn[0]._text;
+                                    var county = item.fylkesnavn[0]._text;
+                                    var wkid = item.epsgKode[0]._text;
+                                    var unprojectedGeometry = new geometry_1.Point({
+                                        x: x, y: y, spatialReference: { wkid: wkid }
+                                    });
+                                    var geometry = projection.project(unprojectedGeometry, view.spatialReference);
+                                    var attributes = { x: x, y: y, name: name, ssrId: ssrId, status: status, SpName: SpName, type: type, municipality: municipality, county: county, wkid: wkid };
+                                    var popupTemplate = new PopupTemplate({
+                                        title: "{name}, {municipality}, {county}",
+                                        content: [{
+                                                type: "fields",
+                                                fieldInfos: [{
+                                                        fieldName: "type",
+                                                        label: "Property Type"
+                                                    }, {
+                                                        fieldName: "municipality",
+                                                        label: "Municipality"
+                                                    }, {
+                                                        fieldName: "county",
+                                                        label: "County"
+                                                    }, {
+                                                        fieldName: "status",
+                                                        label: "Status"
+                                                    }, {
+                                                        fieldName: "y",
+                                                        label: "latitude"
+                                                    }, {
+                                                        fieldName: "x",
+                                                        label: "longitude"
+                                                    }]
+                                            }]
+                                    });
+                                    var feature = new Graphic({ geometry: geometry, attributes: attributes, popupTemplate: popupTemplate });
+                                    var extent = new geometry_1.Extent({
+                                        xmax: geometry.x + 1000,
+                                        xmin: geometry.x - 1000,
+                                        ymax: geometry.y + 1000,
+                                        ymin: geometry.y - 1000,
+                                        spatialReference: geometry.spatialReference.clone()
+                                    });
+                                    return {
+                                        name: name + ", " + municipality + ", " + county,
+                                        feature: feature,
+                                        extent: extent
+                                    };
+                                });
+                                console.log("results: ", searchResults);
+                                if (suggestMunicipality) {
+                                    searchResults = searchResults.filter(function (result) {
+                                        return result.feature.attributes.municipality === suggestMunicipality;
+                                    });
+                                }
+                                if (suggestCounty) {
+                                    searchResults = searchResults.filter(function (result) {
+                                        return result.feature.attributes.county === suggestCounty;
+                                    });
+                                }
+                                return [2 /*return*/, searchResults];
+                        }
+                    });
+                });
+            }
+            var map, view, norwayPlaceNameSearchLocator, search;
+            var _this = this;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, projection.load()];
+                    case 1:
+                        _a.sent();
+                        map = new EsriMap({
+                            basemap: "streets"
+                        });
+                        view = new MapView({
+                            map: map,
+                            container: "viewDiv",
+                            zoom: 4,
+                            center: [15, 65],
+                            popup: {
+                                dockEnabled: true,
+                                dockOptions: {
+                                    position: "bottom-left",
+                                    breakpoint: false,
+                                    buttonEnabled: false
+                                }
+                            }
+                        });
+                        // const propertyName = "ekenes";
+                        return [4 /*yield*/, view.when()];
+                    case 2:
+                        // const propertyName = "ekenes";
+                        _a.sent();
+                        norwayPlaceNameSearchLocator = new SearchSource({
+                            placeholder: "example: Ekenes",
+                            getSuggestions: function (params) {
+                                return __awaiter(this, void 0, void 0, function () {
+                                    var suggestTerm, sourceIndex, searchResults;
+                                    return __generator(this, function (_a) {
+                                        switch (_a.label) {
+                                            case 0:
+                                                console.log("suggestions: ", params);
+                                                suggestTerm = params.suggestTerm, sourceIndex = params.sourceIndex;
+                                                return [4 /*yield*/, searchPlaceNames(suggestTerm)];
+                                            case 1:
+                                                searchResults = _a.sent();
+                                                // searchTerm = suggestTerm;
+                                                // console.log("suggestions: ", suggestTerm);
+                                                return [2 /*return*/, searchResults.map(function (result) {
+                                                        var _a = result.feature.attributes, name = _a.name, municipality = _a.municipality, county = _a.county;
+                                                        return {
+                                                            key: name,
+                                                            text: name + ", " + municipality + ", " + county,
+                                                            sourceIndex: sourceIndex
+                                                        };
+                                                    })];
+                                        }
+                                    });
+                                });
+                            },
+                            getResults: function (params) { return __awaiter(_this, void 0, void 0, function () {
+                                var propertyName, terms, municipality, county;
+                                return __generator(this, function (_a) {
+                                    switch (_a.label) {
+                                        case 0:
+                                            console.log("results params", params);
+                                            propertyName = params.suggestResult.key || params.suggestResult.text;
+                                            terms = params.suggestResult.text.split(",");
+                                            municipality = null;
+                                            county = null;
+                                            if (terms.length > 1) {
+                                                municipality = terms[1].trim();
+                                                county = terms[2].trim();
+                                            }
+                                            console.log(terms);
+                                            return [4 /*yield*/, searchPlaceNames(propertyName, municipality, county)];
+                                        case 1: return [2 /*return*/, _a.sent()];
+                                    }
+                                });
+                            }); },
+                            popupTemplate: new PopupTemplate({
+                                title: "{name}, {municipality}, {county}",
+                                content: [{
+                                        type: "fields",
+                                        fieldInfos: [{
+                                                fieldName: "type",
+                                                label: "Property Type"
+                                            }, {
+                                                fieldName: "municipality",
+                                                label: "Municipality"
+                                            }, {
+                                                fieldName: "county",
+                                                label: "County"
+                                            }, {
+                                                fieldName: "status",
+                                                label: "Status"
+                                            }, {
+                                                fieldName: "y",
+                                                label: "latitude"
+                                            }, {
+                                                fieldName: "x",
+                                                label: "longitude"
+                                            }]
+                                    }]
+                            })
+                        });
+                        search = new Search({
+                            view: view,
+                            includeDefaultSources: false,
+                            sources: [norwayPlaceNameSearchLocator],
+                            maxSuggestions: 50,
+                        });
+                        search.viewModel.on("select-result", function (e) {
+                            console.log(e);
+                        });
+                        view.ui.add(search, "top-right");
+                        return [2 /*return*/];
+                }
+            });
+        }); })();
+    }
+    catch (error) {
+        console.error(error);
+    }
+});
+//# sourceMappingURL=main.js.map
---
+++ widgets/search/custom-source/app/main.ts
@@ -0,0 +1,224 @@
+import esri = __esri;
+
+import EsriMap = require("esri/Map");
+import MapView = require("esri/views/MapView");
+
+import Search = require("esri/widgets/Search");
+import esriRequest = require("esri/request");
+import SearchSource = require("esri/widgets/Search/SearchSource");
+import geometryEngine = require("esri/geometry/geometryEngine");
+import projection = require("esri/geometry/projection");
+
+import PopupTemplate = require("esri/PopupTemplate");
+
+import Graphic = require("esri/Graphic");
+
+import { Point, Extent } from "esri/geometry";
+
+try {
+  ( async () => {
+
+    await projection.load();
+
+    const map = new EsriMap({
+      basemap: "streets"
+    });
+
+    const view = new MapView({
+      map: map,
+      container: "viewDiv",
+      zoom: 4,
+      center: [15, 65],
+      popup: {
+        dockEnabled: true,
+        dockOptions: {
+          position: "bottom-left",
+          breakpoint: false,
+          buttonEnabled: false
+        }
+      }
+    });
+
+    // const propertyName = "ekenes";
+
+    await view.when();
+
+    const norwayPlaceNameSearchLocator = new SearchSource({
+      placeholder: "example: Ekenes",
+
+      getSuggestions: async function (params: esri.GetSuggestionsParameters): Promise<esri.SuggestResult[]> {
+        console.log("suggestions: ", params);
+        const { suggestTerm, sourceIndex } = params;
+        const searchResults = await searchPlaceNames(suggestTerm);
+
+        // searchTerm = suggestTerm;
+
+        // console.log("suggestions: ", suggestTerm);
+        return searchResults.map( result => {
+          const { name, municipality, county } = result.feature.attributes;
+
+          return {
+            key: name,
+            text: `${name}, ${municipality}, ${county}`,
+            sourceIndex
+          }
+        });
+      },
+
+      getResults: async (params: esri.GetResultsParameters) => {
+        console.log("results params", params);
+        const propertyName = params.suggestResult.key || params.suggestResult.text;
+        const terms = params.suggestResult.text.split(",");
+
+        let municipality:string = null;
+        let county:string = null;
+        if(terms.length > 1){
+          municipality = terms[1].trim();
+          county = terms[2].trim();
+        }
+        
+        console.log(terms);
+        return await searchPlaceNames(propertyName, municipality, county);
+      },
+
+      popupTemplate: new PopupTemplate({
+        title: "{name}, {municipality}, {county}",
+        content: [{
+          type: "fields",
+          fieldInfos: [{
+            fieldName: "type",
+            label: "Property Type"
+          }, {
+            fieldName: "municipality",
+            label: "Municipality"
+          }, {
+            fieldName: "county",
+            label: "County"
+          }, {
+            fieldName: "status",
+            label: "Status"
+          }, {
+            fieldName: "y",
+            label: "latitude"
+          }, {
+            fieldName: "x",
+            label: "longitude"
+          }]
+        }]
+      })
+    } as any);
+
+    async function searchPlaceNames (propertyName: string, suggestMunicipality?: string, suggestCounty?: string): Promise<esri.SearchResult[]> {
+      
+      const searchUrl = `https://ws.geonorge.no/SKWS3Index/ssr/sok`;
+
+      const searchResponse = await esriRequest(searchUrl, {
+        responseType: "xml",
+        query: {
+          navn: propertyName
+        }
+      });
+
+      // console.log(searchResponse);
+
+      const xmlResponse = searchResponse.data;
+      const jsResponse = window.xmlToJSON.parseXML(xmlResponse);
+      console.log("js response", jsResponse);
+
+      const rawResults = jsResponse.sokRes[0].stedsnavn;
+
+      let searchResults: esri.SearchResult[] = rawResults.map( (item: any) => {
+        const x = item.aust[0]._text;
+        const y = item.nord[0]._text;
+        const name = item.stedsnavn[0]._text;
+        const ssrId = item.ssrId[0]._text;
+        const status = item.skrivemaatestatus[0]._text;
+        const SpName = item.skrivemaatenavn[0]._text;
+        const type = item.navnetype[0]._text;
+        const municipality = item.kommunenavn[0]._text;
+        const county = item.fylkesnavn[0]._text;
+        const wkid = item.epsgKode[0]._text;
+        
+        const unprojectedGeometry = new Point({
+          x, y, spatialReference: { wkid }
+        });
+
+        const geometry = projection.project(unprojectedGeometry, view.spatialReference) as esri.Point;
+
+        const attributes = { x, y, name, ssrId, status, SpName, type, municipality, county, wkid };
+        const popupTemplate = new PopupTemplate({
+          title: "{name}, {municipality}, {county}",
+          content: [{
+            type: "fields",
+            fieldInfos: [{
+              fieldName: "type",
+              label: "Property Type"
+            }, {
+              fieldName: "municipality",
+              label: "Municipality"
+            }, {
+              fieldName: "county",
+              label: "County"
+            }, {
+              fieldName: "status",
+              label: "Status"
+            }, {
+              fieldName: "y",
+              label: "latitude"
+            }, {
+              fieldName: "x",
+              label: "longitude"
+            }]
+          }]
+        });
+        const feature = new Graphic({ geometry, attributes, popupTemplate });
+        const extent = new Extent({
+          xmax: geometry.x + 1000,
+          xmin: geometry.x - 1000,
+          ymax: geometry.y + 1000,
+          ymin: geometry.y - 1000,
+          spatialReference: geometry.spatialReference.clone()
+        });
+
+        return { 
+          name: `${name}, ${municipality}, ${county}`,
+          feature, 
+          extent 
+        };
+         
+      });
+
+      console.log("results: ", searchResults);
+
+      if(suggestMunicipality){
+        searchResults = searchResults.filter( result => {
+          return result.feature.attributes.municipality === suggestMunicipality;
+        });
+      }
+
+      if(suggestCounty){
+        searchResults = searchResults.filter( result => {
+          return result.feature.attributes.county === suggestCounty;
+        });
+      }
+
+      return searchResults;
+    }
+
+    const search = new Search({
+      view,
+      includeDefaultSources: false,
+      sources: [ norwayPlaceNameSearchLocator ],
+      maxSuggestions: 50,
+    });
+
+    search.viewModel.on("select-result", (e) => {
+      console.log(e);
+    })
+
+    view.ui.add(search, "top-right");
+
+  })();
+} catch (error){ 
+  console.error(error);
+}
